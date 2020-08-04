c2ca1fba03a83ccaf9d13b86ccb46dcfbd08fd20
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2020-08-04 22:22:49 +0100

    Added error handling page and removed redundant code

--- app/routes.js
+++ app/routes.js
@@ -4,26 +4,24 @@ const apiController = require('./controllers/ApiController')
 
 // Add your routes here - above the module.exports line
 
-router.get('/api', function (req, res) {
-  //apiController.getAllSkills().then( data => res.json(data)); -- to delete later
-  apiController.getAllSkillsFamily().then(data => res.json(data));
+router.get('/api', function (req, res, next) {
+  apiController.getAllSkillsFamily()
+  .then(data => res.json(data))
+  .catch((error) => next(error));
 })
 
-// Not entirely sure I need the following route
-
-router.get('/api/flat', function (req, res) {
-  apiController.getAllSkills(true).then( data => res.json(data));
+router.get('/table', function (req, res, next) {
+  apiController.getAllSkillsForAllFamiliesTableView(true)
+  .then( data => res.render('table', {data: data}))
+  .catch((error) => next(error));
 })
 
-router.get('/table', function (req, res) {
-  apiController.getAllSkillsForAllFamiliesTableView(true).then( data => res.render('table', {data: data}));
-})
-
-router.get('/csv', function (req, res) {
+router.get('/csv', function (req, res, next) {
   res.setHeader('Content-Type', 'text/csv');
   res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'skills-download-' + Date.now() + '.csv\"');
-  // apiController.getAllSkillsCsv().then( data => res.send(data)); -- to delete later
-  apiController.getAllSkillsFamilyCsv().then( data => res.send(data));
+  apiController.getAllSkillsFamilyCsv()
+  .then( data => res.send(data))
+  .catch((error) => next(error));
 })
 
 module.exports = router
